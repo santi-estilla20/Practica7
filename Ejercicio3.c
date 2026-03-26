ecf1e3c2ac9db0a85e1736ce581299ab00216aa0
santi-estilla20 <os967577@gmail.com>
2026-03-26 13:47:39 -0600

    Update Ejercicio3.c

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -4,9 +4,9 @@ int main()
  int num;
  printf("Ingrese un número:\n");
  scanf("%d",&num);
- if ( num%2 == 0 )
-   printf("El número %d es par.\n",num);
- else
+ if ( num%2 )
    printf("El número %d es impar.\n",num);
+ else
+   printf("El número %d es par.\n",num);
  return 0;
 }
